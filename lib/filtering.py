bf68620e09b27a23a2d3bf7b12df0dd87b25fb99
Micah <micahgcnavia@gmail.com>
2025-04-25 17:12:34 -0300

    relies on configuration parameters

--- lib/filtering.py
+++ lib/filtering.py
@@ -3,10 +3,12 @@ import numpy as np
 from glob import glob
 from natsort import os_sorted
 import os
+import ast
 from tqdm import tqdm
 from more_itertools import pairwise
 from functools import reduce
 from collections import defaultdict
+from get_config import *
 
 class Filter():
 
@@ -15,13 +17,13 @@ class Filter():
 
     """
 
-    def __init__(self, database_path, target, delta_params=None):
+    def __init__(self, delta_params=None):
 
         """
             Initializes the Filter class.
             
-            :param database_path: Path of the folder that contains all models.
-            :type database_path: str
+            :param library: Name of the model library to search for in the database folder.
+            :type library: str
             :param target: DataFrame containing the name of the object and its parameters.
             :type target: pandas.DataFrame
             :param delta_params: Optional DataFrame describing the parameters steps for the model.
@@ -30,25 +32,33 @@ class Filter():
         """
 
         print('='*27+' Initializing Filter '+'='*27+'\n')
+
+        # Getting user setup data
+        config = get_config()
+
+        targets_path = config['USER_DATA']['targets_path']
+        targets = pd.read_csv(targets_path)
+        database_path = config['USER_DATA']['database_path']
+        self.library = config['USER_DATA']['library_name'].lower()
+        self.delta_params = ast.literal_eval(config['USER_DATA']['delta_params']) # Getting the dictionary
+        #self.save_final_spectra = config.getboolean('SETTINGS', 'save_final_spectra')
+
         self.cwd = os.getcwd()
-        self.database_path = database_path
-        self.models_list = sorted(glob(self.database_path+'*'))
-        self.target = target
+        self.models_list = sorted(glob(database_path+self.library+'/*'))
+        target = targets.loc[targets['star'] == 'CoRoT-1'] # This will be replaced by a loop in the future
         self.params = {
-            'teff': self.target['teff'].item(),
-            'logg': self.target['logg'].item(),
-            'feh': self.target['feh'].item()
+            'teff': target['teff'].item(),
+            'logg': target['logg'].item(),
+            'feh': target['feh'].item()
         }
         self.parameters = list(self.params.keys())
-        self.delta_params = delta_params or {
-            'teff': 100,  # [K]
-            'logg': 0.5,  # [dex]
-            'feh': 0.5    # [dex]
-        }
+        objct = list(target.columns)[0]
+        self.name = target[objct].item().strip()
         self.interpolate = {
             'teff': None,
             'logg': None,
             'feh': None}
+        # The conditions to filter the database models will be storaged in this list
         self.conditions = []
 
 
@@ -65,29 +75,31 @@ class Filter():
             :rtype: dict
         
         """
-        
+    
         file = open(file_path, mode='r', newline='')
         lines = file.readlines()
         header = lines[0:5]
         
+        # Getting the parameters' names
         param_name = [header[i].split(' ')[1] for i in np.arange(1,len(header))]
         
         header_dict = {}
 
         for i, param in zip(np.arange(1,len(header)), param_name):
         
+            # Retrieving the values
             header_dict[param] = float(header[i].split(' ')[3])
             
-        if show_model:
-            
-            if header[0].split(' ')[1] == 'BT-NextGen':
-                
-                print(header[0].split(' ')[1]+' '+header[0].split(' ')[2].split('\n')[0])
-                
-            else:
-                
-                print(header[0].split(' ')[1])
+        model_names = header[0].split(' ')
 
+        model = model_names[1].strip()
+
+        if len(model_names) > 2:
+
+            for name in model_names[2:]:
+                model += '_'+name.strip()
+
+        header_dict['model'] = model
         header_dict['path'] = file_path
 
         if header_dict['alpha'] != 0:
@@ -100,10 +112,10 @@ class Filter():
 
         return header_dict
 
-    def get_all_models(self, save=False):
+    def get_all_models(self, save=None):
 
         """
-            Stores all models in a pandas DataFrame.
+            Storages all models in a pandas DataFrame.
             
             :param save: Flag to indicate whether to save the DataFrame in a CSV file.
             :type save: bool
@@ -120,7 +132,7 @@ class Filter():
 
         if save:
 
-            df.to_csv('all_models.csv', index=False)
+            df.to_csv(self.cwd+'/all_models_'+self.library+'.csv', index=False)
 
         return df
 
@@ -132,11 +144,14 @@ class Filter():
 
             :param df: DataFrame with the metadata of all models in the database.
             :type df: pandas.DataFrame
-            :param param: Name of the parameter to filter with.
+            :param param: Name of the parameter to filter by.
             :type param: str
         
         """
 
+        update_database = {}
+
+        # Checks if the input parameter value matches one of the models' default values
         if self.params[param] in list(df[param]):
 
             self.interpolate[param] = False
@@ -145,43 +160,108 @@ class Filter():
         else:
 
             self.interpolate[param] = True
+            # Gets values 1 step around the input parameter value
             self.conditions.append(abs(df[param] - self.params[param]) <= self.delta_params[param])
 
-    def filter_all(self):
+    def filter_all(self, save_all_models=False, save_result=False):
 
         """
-        Applies the filtering for the target and its parameters. Creates auxiliary files for the interpolation script.
+            Applies the filtering for the target and its parameters. Creates auxiliary files for the interpolation script.
+
+            :param save_all_models: Flag to indicate whether to save the table with all models' information.
+            :type save_all_models: bool
+            :param save_result: Flag to indicate whether to save the filtered table and the instructions table for interpolation.
+            :type save_result: bool
+            :return: A table containing all spectra needed for interpolation and a boolean table indicating which parameters to interpolate.
+            :rtype: pandas.DataFrame
 
         """
 
-        all_models = self.get_all_models()
+        try:
 
-        for param in self.parameters:
+            # Reads the file if it already exists
+            all_models = pd.read_csv(self.cwd+f'/all_models_{self.library}.csv')
+
+        except:
+
+            # If there is no all_models file, creates one
+            all_models = self.get_all_models(save=save_all_models)
+
+        print(f'\nFiltering the database to get spectra for {self.name}\n')
+
+        print(f'{self.name} parameters:\n')
+
+        for key, item in self.params.items():
+
+            print(key+f' = {item}')
+
+        for param in (self.parameters):
 
             self.filter_param(all_models, param)
 
-        filtered_df = all_models[reduce(lambda x, y: x & y, self.conditions)]
+        filtered_df = all_models[reduce(lambda x, y: x & y, self.conditions)] # Makes sure all conditions are satisfied
 
-        interpolate = pd.DataFrame(self.interpolate, index=[0])
+        interpolate = pd.DataFrame(self.interpolate, index=[0]) # This will be a simple DataFrame stating which parameters to interpolate
+
+        if save_result:
+
+            name = self.name.lower()
+
+            filtered_df.to_csv(self.cwd+f'/output/filtered/{name}_data.csv', index=False)
+            interpolate.to_csv(self.cwd+f'/output/filtered/{name}_interpolate.csv', index=False)
         
         return filtered_df, interpolate
 
+    def check_spectra_availability(self, interpolate_flags, filtered_df):
+
+        """
+            Checks whether we have all spectra needed for interpolation based on the number of parameters to interpolate.
+
+            :param interpolate_flags: DataFrame specifing which parameters to interpolate based on bool values.
+            :type interpolate_flags: pandas.DataFrame
+            :param filtered_df: DataFrame containing all spectra needed for interpolation and their parameters' values.
+            :type filtered_df: pandas.DataFrame
+            :return: True of False
+            :rtype: bool
+
+        """
+
+        print('\nChecking spectra availability...')
+        n = interpolate_flags.sum().sum() # number of parameters to interpolate
+
+        N = len(filtered_df) # number of spectra available
+
+        if N != 2**n:
+            print(f'Missing {2**n - N} spectra for {self.name}')
+            return False
+
+        else:
+            print('All spectra available!')
+            return True
+
 def main():
 
-    cwd = os.getcwd()
-    database_path = cwd+'/database/'
+    # Starts Filter class
+    filter = Filter()
 
-    targets = pd.read_csv('stars.csv') # List of objects to interpolate
+    filtered_df, interpolate = filter.filter_all(save_all_models=False, save_result=False)
 
-    target = targets.loc[targets['star'] == 'CoRoT-1 ']
+    print('\nParameters to interpolate:\n')
 
-    filter = Filter(database_path=database_path, target=target)
+    for param in list(interpolate.columns):
 
+        if interpolate[param].item():
+            print('->', param)
 
-    filtered_df, interpolate = filter.filter_all()
-    print(filtered_df)
-    print(interpolate)
+    if filter.check_spectra_availability(interpolate, filtered_df):
 
+        print('\nSpectra needed for interpolation:\n')
+        print(filtered_df)
+
+        print('='*34+' Finish '+'='*34+'\n')
+
+    else:
+        pass
 
 if __name__ == "__main__":
     main()
